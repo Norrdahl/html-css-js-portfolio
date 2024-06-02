dfd2a61a4ae996fe69c8902272feb243e471eb08
Alexander Norrdahl <128329455+Norrdahl@users.noreply.github.com>
2024-06-02 04:06:30 +0300

    changes + dark-lightmode switch

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu(){
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const checkbox = document.getElementById('light-dark-switch')
+    const root = document.documentElement;
+
+    checkbox.addEventListener('change', () => {
+        if (checkbox.checked) {
+            root.style.setProperty('color-scheme', 'normal');
+        } else {
+            root.style.setProperty('color-scheme', 'dark')
+        }
+    });
+
+    if (checkbox.checked) {
+        root.style.setProperty('color-scheme', 'normal');
+    } else {
+        root.style.setProperty('color-scheme', 'dark')
+    }
+})
